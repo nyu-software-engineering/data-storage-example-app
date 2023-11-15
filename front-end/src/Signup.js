b5370c9d391623c1af295862416878cc67c3c967
bloombar <amos@cs.nyu.edu>
2023-11-15 17:06:14 -0500

    removing unused var

--- front-end/src/Signup.js
+++ front-end/src/Signup.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from "react"
-import { Link, Navigate, useSearchParams } from "react-router-dom"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { Link, Navigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
 // import logo from './logo.svg';
-import "./Login.css"
+import "./Login.css";
 
 const Signup = props => {
-  let [urlSearchParams] = useSearchParams() // get access to the URL query string parameters
+  // let [urlSearchParams] = useSearchParams() // get access to the URL query string parameters
 
   // create state variables to hold username and password
-  const [response, setResponse] = useState({}) // the API will return an object with a JWT token, if the user logs in successfully
-  const [errorMessage, setErrorMessage] = useState("")
+  const [response, setResponse] = useState({}); // the API will return an object with a JWT token, if the user logs in successfully
+  const [errorMessage, setErrorMessage] = useState("");
 
   // if the user's logged-in status changes, save the token we receive from the server
   useEffect(() => {
     // if the user is logged-in, save the token to local storage
     if (response.success && response.token) {
-      console.log(`User successfully logged in: ${response.username}`)
-      localStorage.setItem("token", response.token) // store the token into localStorage
+      console.log(`User successfully logged in: ${response.username}`);
+      localStorage.setItem("token", response.token); // store the token into localStorage
     }
-  }, [response])
+  }, [response]);
 
   // what to do when the user clicks the submit buton on the form
   const handleSubmit = async e => {
     // prevent the HTML form from actually submitting... we use React's javascript code instead
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       // create an object with the data we want to send to the server
       const requestData = {
         username: e.target.username.value, // gets the value of the field in the submitted form with name='username'
         password: e.target.password.value, // gets the value of the field in the submitted form with name='password',
-      }
+      };
       // send a POST request with the data to the server api to authenticate
       const response = await axios.post(
         `${process.env.REACT_APP_BACKEND}/auth/signup`,
         requestData
-      )
+      );
       // store the response data into s the data state variable
-      console.log(`Server response: ${JSON.stringify(response.data, null, 0)}`)
-      setResponse(response.data)
+      console.log(`Server response: ${JSON.stringify(response.data, null, 0)}`);
+      setResponse(response.data);
     } catch (err) {
       // request failed... user entered invalid credentials
       setErrorMessage(
         "The username or password you entered are not valid.  Try harder! "
-      )
+      );
     }
-  }
+  };
 
   // if the user is not logged in, show the signup form
   if (!response.success)
@@ -74,10 +74,10 @@ const Signup = props => {
           </p>
         </section>
       </div>
-    )
+    );
   // otherwise, if the user has successfully logged-in, redirect them to a different page
   // in this example, we simply redirect to the home page, but a real app would redirect to a page that shows content only available to logged-in users
-  else return <Navigate to="/" />
-}
+  else return <Navigate to="/" />;
+};
 
-export default Signup
+export default Signup;
