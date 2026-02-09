414724d70849dd76aaf27672622db8b4be4e07c9
bloombar <2209098+bloombar@users.noreply.github.com>
2026-02-09 17:20:28 -0500

    udpated to latest react

--- front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var react_1 = require("react");
-var react_dom_1 = require("react-dom");
-require("./index.css");
-var App_1 = require("./App");
-react_dom_1.default.render((0, jsx_runtime_1.jsx)(react_1.default.StrictMode, { children: (0, jsx_runtime_1.jsx)(App_1.default, {}) }), document.getElementById('root'));
